645f2d9c7d143944904d1b2095f1ad9a7bc7d19e
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-24 09:54:00 +0100

    error handling

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from "express";
 import Stripe from "stripe";
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 3000;
 
 app.post("/checkout", async (req, res) => {
@@ -23,26 +30,37 @@ app.post("/checkout", async (req, res) => {
 
 app.get("/getPrice", async (req, res) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-  const productName = decodeURIComponent((req.query.name || ""));
-  const products = await stripe.products.list();
-  const product = products.data.find((o) => o.name === productName);
-  if (!product) {
-    res.status(404).json({ error: "Product not found" });
+  const productName = decodeURIComponent(req.query.name);
+  try {
+    const products = await stripe.products.list();
+    const product = products.data.find((o) => o.name === productName);
+    if (!product) {
+      res.status(404).json({ error: "Product not found" });
+    }
+    if (!product.default_price) {
+      res.status(404).json({ error: "Price not found" });
+    }
+    const price = await stripe.prices.retrieve(product.default_price);
+    res.json({
+      price_in_cents: price.unit_amount,
+      price_id: price.id,
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  if (!product.default_price) {
-    res.status(404).json({ error: "Price not found" });
-  }
-  const price = await stripe.prices.retrieve(product.default_price);
-  res.json({
-    price_in_cents: price.unit_amount,
-    price_id: price.id,
-  });
+
 });
 
 app.get("/getProducts", async (req, res) => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-  const products = await stripe.products.list();
-  res.json(products);
+  try {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    const products = await stripe.products.list();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
 });
 
 app.listen(PORT, () => {
